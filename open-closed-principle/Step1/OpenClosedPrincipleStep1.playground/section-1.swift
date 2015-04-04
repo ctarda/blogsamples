697f91e20ae0f623d3776f3a426404d5a0e4aaff
Cesar Tardaguila <cesar.tardaguila@accedo.tv>
2015-04-04 17:26:40 +0200

    Implement sample to Open Closed Principle

--- open-closed-principle/Step1/OpenClosedPrincipleStep1.playground/section-1.swift
+++ open-closed-principle/Step1/OpenClosedPrincipleStep1.playground/section-1.swift
@@ -1,9 +1,13 @@
+/*
+    The backend provides a Program object in the services response. Also, it provides a "type" that identifies the type of program.
+*/
 enum ProgramType {
     case Movie
     case Episode
     case LiveProgram
 }
 
+// Simplified Program, for the shake of the example.
 class Program {
     private(set) var title: String
     private(set) var type: ProgramType
@@ -13,7 +17,10 @@ class Program {
         self.type = type
     }
 }
+
 /*
+    Function to be called when the user taps a program. This shall launch the a different view controller according to the content that needs to be displayed.
+*/
 func navigateToDetails(program: Program) {
     switch program.type {
     case .Movie:
@@ -24,55 +31,9 @@ func navigateToDetails(program: Program) {
         println("It is a LiveProgram, navigate to Live Program Details")
     }
 }
-*/
-///
 
-protocol Router {
-    func canHandleProgram(program: Program)->Bool
-    func navigateTo(program: Program)
-}
-
-final class MovieRouter: Router {
-    func canHandleProgram(program: Program)->Bool {
-        return program.type == .Movie
-    }
-    
-    func navigateTo(program: Program) {
-        println("It is a movie, navigate to Movie Details")
-    }
-}
-
-final class EpisodeRouter: Router {
-    func canHandleProgram(program: Program)->Bool {
-        return program.type == .Episode
-    }
-    
-    func navigateTo(program: Program) {
-        println("It is an Episode, navigate to Episode Details")
-    }
-}
-
-final class LiveProgramRouter: Router {
-    func canHandleProgram(program: Program)->Bool {
-        return program.type == .LiveProgram
-    }
-    
-    func navigateTo(program: Program) {
-        println("It is a LiveProgram, navigate to Live Program Details")
-    }
-}
-
-func navigateToDetails(program: Program, routers:[Router]) {
-    let matchingRouters = routers.filter({$0.canHandleProgram(program)})
-    if let router = matchingRouters.first {
-        router.navigateTo(program)
-    }
-}
-
-let routers:[Router] = [MovieRouter(), EpisodeRouter(), LiveProgramRouter()]
 let sampleMovie = Program(title: "The Quiet Man", type: .Movie)
-let sampleEpisode = Program(title: "CSI: S43E02", type: .Episode)
+let sampleEpisode = Program(title: "CSI: S47E03", type: .Episode)
 
-navigateToDetails(sampleMovie, routers)
-navigateToDetails(sampleEpisode, routers)
-
+navigateToDetails(sampleMovie)
+//navigateToDetails(sampleEpisode)
--- open-closed-principle/Step2/OpenClosedPrincipleStep2.playground/section-1.swift
+++ open-closed-principle/Step2/OpenClosedPrincipleStep2.playground/section-1.swift
@@ -1,9 +1,13 @@
+/*
+The backend provides a Program object in the services response. Also, it provides a "type" that identifies the type of program.
+*/
 enum ProgramType {
     case Movie
     case Episode
     case LiveProgram
 }
 
+// Simplified Program, for the shake of the example.
 class Program {
     private(set) var title: String
     private(set) var type: ProgramType
@@ -13,25 +17,14 @@ class Program {
         self.type = type
     }
 }
-/*
-func navigateToDetails(program: Program) {
-    switch program.type {
-    case .Movie:
-        println("It is a movie, navigate to Movie Details")
-    case .Episode:
-        println("It is an Episode, navigate to Episode Details")
-    case .LiveProgram:
-        println("It is a LiveProgram, navigate to Live Program Details")
-    }
-}
-*/
-///
 
+// Protocol that abstract the behaviour: navigating to the view that contains the Program's extended info.
 protocol Router {
     func canHandleProgram(program: Program)->Bool
     func navigateTo(program: Program)
 }
 
+// Implementation of the Router protocol that navigates to the Movie extended info
 final class MovieRouter: Router {
     func canHandleProgram(program: Program)->Bool {
         return program.type == .Movie
@@ -42,6 +35,7 @@ final class MovieRouter: Router {
     }
 }
 
+// Implementation of the Router protocol that navigates to the Episode extended info
 final class EpisodeRouter: Router {
     func canHandleProgram(program: Program)->Bool {
         return program.type == .Episode
@@ -52,6 +46,7 @@ final class EpisodeRouter: Router {
     }
 }
 
+// Implementation of the Router protocol that navigates to the LiveProgram extended info
 final class LiveProgramRouter: Router {
     func canHandleProgram(program: Program)->Bool {
         return program.type == .LiveProgram
@@ -62,6 +57,11 @@ final class LiveProgramRouter: Router {
     }
 }
 
+/*
+    Function to be called when the user taps a program. All logic related to navigation has been removed from this function. A naive implementation of this method would loop the routers array, and when it finds one that responds true to canHandleProgram, it would call navigateTo on it.
+
+    Here, we use the first element returned by filter. If we add more routers, we do not need to touch this method. If the implementation of Program changes we do not need to touch this method either.
+*/
 func navigateToDetails(program: Program, routers:[Router]) {
     let matchingRouters = routers.filter({$0.canHandleProgram(program)})
     if let router = matchingRouters.first {
@@ -74,5 +74,5 @@ let sampleMovie = Program(title: "The Quiet Man", type: .Movie)
 let sampleEpisode = Program(title: "CSI: S43E02", type: .Episode)
 
 navigateToDetails(sampleMovie, routers)
-navigateToDetails(sampleEpisode, routers)
+//navigateToDetails(sampleEpisode, routers)
 
